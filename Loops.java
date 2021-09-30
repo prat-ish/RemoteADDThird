215b90b9f360ddc48b1af137e9d7de7804a3cd00
Pratish <psnanandkar@gmail.com>
2021-09-30 12:48:30 +0530

    Fourth Remote Push

--- Loops.java
+++ Loops.java
@@ -2,7 +2,7 @@ public class Loops{
     public static void main(String[] args){
         int num =10;
         for(int i=0; i<num; i++){
-            System.out.println("Hello World... "+i);
+            System.out.println("Hello World Again... "+i);
         }
     }
 }
