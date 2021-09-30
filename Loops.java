8c46056c0519ceb6e6e33214def7d13d799e345c
Pratish <psnanandkar@gmail.com>
2021-09-30 12:32:05 +0530

    Third REMOTE ADD?

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops{
+    public static void main(String[] args){
+        int num =10;
+        for(int i=0; i<num; i++){
+            System.out.println("Hello World... "+i);
+        }
+    }
+}
